ddfa1359be56000314120645d30dfb41dc2b4f7a
Jizzyyy <kadhafinaufal2@gmail.com>
2026-08-11 16:30:00 +0700

    refactor(map): extract user location marker to dedicated widget

---
+++ lib/src/features/map/presentation/widgets/user_location_marker.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class UserLocationMarker extends StatelessWidget {
+  const UserLocationMarker({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final primary = Theme.of(context).colorScheme.primary;
+
+    return Container(
+      decoration: BoxDecoration(
+        color: primary,
+        shape: BoxShape.circle,
+        border: Border.all(color: Colors.white, width: 3),
+        boxShadow: [
+          BoxShadow(
+            color: primary.withOpacity(0.3),
+            blurRadius: 8,
+            spreadRadius: 2,
+          ),
+        ],
+      ),
+    );
+  }
+}
