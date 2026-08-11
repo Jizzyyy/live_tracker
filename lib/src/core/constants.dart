2268f72ffb8af99bd7a3761527da8ac2bcd467f6
Jizzyyy <kadhafinaufal2@gmail.com>
2026-08-11 17:00:00 +0700

    refactor: extract map config to centralized constants

---
+++ lib/src/core/constants.dart
@@ -0,0 +1,9 @@
+import 'package:latlong2/latlong.dart';
+
+class MapDefaults {
+  static const initialCenter = LatLng(-6.200000, 106.816666);
+  static const initialZoom = 13.0;
+  static const focusedZoom = 16.0;
+  static const tileUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+  static const packageName = 'com.example.live_tracker';
+}
